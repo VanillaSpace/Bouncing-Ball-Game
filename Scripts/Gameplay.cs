abc3a90ca1a663269dc7bc07a8b31f9118123297
VanillaSpace <aohashi@live.com>
2019-09-07 19:28:09 -0400

    Add files via upload

---
+++ Scripts/Gameplay.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gameplay : MonoBehaviour
+{
+    public Text scoreTxt;
+    private int scoreboard = 0;
+    public string preTextScore = "Score: ";
+    private string scoreTx = "0";
+    public int pointsPerBounce = 35;
+
+
+
+    public float minTranslation = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 colPos = collision.transform.position;
+        Rigidbody rigid = GetComponent<Rigidbody>();
+    
+        if (collision.gameObject.tag == "paddle")
+        {
+            float diffx = minTranslation + (colPos.x - transform.position.x) * Random.Range(1, 10);
+            float diffz = minTranslation + (colPos.z - transform.position.z) * Random.Range(1, 10);
+
+            rigid.velocity = new Vector3(-diffx, 8, -diffz);
+            Debug.Log("x: "+ diffx+" z: "+ diffz);
+            AddScore();
+
+            scoreTxt.text = preTextScore + scoreTx;
+        }
+    }
+
+    void AddScore()
+    {
+        scoreboard += pointsPerBounce;
+        scoreTx = scoreboard.ToString();
+    }
+}
---
+++ Scripts/MovePaddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour
+{
+    [SerializeField]
+    private  float speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        float transform_y = (Input.GetAxis("Mouse Y") * speed * Time.deltaTime);
+        float transform_x = (Input.GetAxis("Mouse X") * speed * Time.deltaTime);
+        transform.Translate(transform_x, 0, transform_y);
+    }
+}
